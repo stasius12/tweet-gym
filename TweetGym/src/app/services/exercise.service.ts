713a535d806913ea501b7ff02ac1b0688e23f2d8
Bartlomiej Zgorzynski <b.zgorzynski@gmail.com>
2019-10-26 21:25:22 +0200

    Add exercise service

---
+++ TweetGym/src/app/services/exercise.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { Exercise } from '../models/exercise';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class ExerciseService{
+
+  private baseURL = environment.apiURL;
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getExercises(): Observable<any>{
+    const exercises = this.http.get('http://192.168.0.66:8000/exercises/');
+    return exercises
+  }
+
+  getExerciseDetail(id: number): Observable<any>
+  {
+    const exercises = this.http.get(`${this.baseURL}/exercises/${id}/`);
+    return exercises
+  }
+
+}
