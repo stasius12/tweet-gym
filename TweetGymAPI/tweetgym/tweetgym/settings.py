1759496800011e56eb13770f7a95ad9998ae76b0
Bartlomiej Zgorzynski <b.zgorzynski@gmail.com>
2019-10-26 14:42:39 +0200

    Add corsheaders and djangorestframework

--- TweetGymAPI/tweetgym/tweetgym/settings.py
+++ TweetGymAPI/tweetgym/tweetgym/settings.py
@@ -37,16 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200'
 ]
 
 ROOT_URLCONF = 'tweetgym.urls'
