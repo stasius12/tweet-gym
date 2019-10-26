cc344b7320aa97e664dcc1f268f63aee7d55ec27
Stan <stanislawstepak@outlook.com>
2019-10-26 22:47:32 +0200

    Display user in nice way

--- TweetGymAPI/tweetgym/userprofile/models.py
+++ TweetGymAPI/tweetgym/userprofile/models.py
@@ -7,6 +7,9 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.user.username} <{self.user.email}>"
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
