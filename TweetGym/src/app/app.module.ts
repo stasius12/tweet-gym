b89fa1eb5bab7a6bdcc7487e2546fda865c72f3d
Kamil Wrzesinski <kamilwrze@gmail.com>
2019-10-27 01:47:55 +0200

    new difficulty icons

--- TweetGym/src/app/app.module.ts
+++ TweetGym/src/app/app.module.ts
@@ -8,10 +8,10 @@ import { TESTComponent } from "./test/test.component";
 import { LogInComponent } from "./log-in/log-in.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { ExerciseDetailComponent } from './exercise-detail/exercise-detail.component';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms'
-import { TokenInterceptorService } from './services/token-interceptor.service'
-import { ExcOptionsComponent } from './Components/exc-options/exc-options.component';
+import { ExerciseDetailComponent } from "./exercise-detail/exercise-detail.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TokenInterceptorService } from "./services/token-interceptor.service";
+import { ExcOptionsComponent } from "./Components/exc-options/exc-options.component";
 
 @NgModule({
   declarations: [
@@ -36,7 +36,7 @@ import { ExcOptionsComponent } from './Components/exc-options/exc-options.compon
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptorService,
       multi: true
-    },
+    }
   ],
   bootstrap: [AppComponent]
 })
