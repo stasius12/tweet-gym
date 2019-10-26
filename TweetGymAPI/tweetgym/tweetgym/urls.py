5f66c9e539ef24f460ba9eecceddf1b31f99e07d
Stan <stanislawstepak@outlook.com>
2019-10-26 22:27:48 +0200

    Add token authentication and registration

--- TweetGymAPI/tweetgym/tweetgym/urls.py
+++ TweetGymAPI/tweetgym/tweetgym/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('exercises/', include('exercises.urls')),
     path('users/', include('userprofile.urls')),
+    path('login/', obtain_auth_token, name='api_token_auth'),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
