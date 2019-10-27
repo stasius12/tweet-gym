072c54fb65469c2244561de6c0bbb018359fa054
Stan <stanislawstepak@outlook.com>
2019-10-27 12:45:43 +0100

    Add rating to serialization on exercises

--- TweetGymAPI/tweetgym/exercises/urls.py
+++ TweetGymAPI/tweetgym/exercises/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ExerciseViewSet
+from .views import ExerciseViewSet, ExerciseRatingViewSet
 
 router = routers.DefaultRouter()
-router.register('', ExerciseViewSet)
+router.register('ratings', ExerciseRatingViewSet)
+router.register('exercises', ExerciseViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
