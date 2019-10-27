20552b3fddbd1b19e07e5c779e723f6d072085f6
Bartlomiej Zgorzynski <b.zgorzynski@gmail.com>
2019-10-27 02:44:45 +0100

    Fix Hamburger bug

--- TweetGym/src/app/hamburger-menu/hamburger-menu.component.ts
+++ TweetGym/src/app/hamburger-menu/hamburger-menu.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {starMe, Cardio, HamburgerMe} from './exercise-detail';
+import { AuthService } from '../services/auth.service'
 
 @Component({
   selector: 'app-hamburger-menu',
   templateUrl: './hamburger-menu.component.html',
   styleUrls: ['./hamburger-menu.component.css']
 })
-export class HamburgerMenuComponent implements OnInit {
+export class HamburgerMenuComponent{
 
-  constructor() { }
+  logged: Boolean;
+
+  constructor(
+    private as: AuthService
+  ) { }
 
   ngOnInit() {
     HamburgerMe();
+    this.logged = this.as.isLogged();
+  }
+
+  logout(){
+    this.as.logout();
   }
 
 }
