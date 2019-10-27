8aa2f0ef1f7cc6c4352a555ff2ed3d6d021ea2dc
Stan <stanislawstepak@outlook.com>
2019-10-27 02:38:01 +0100

    Add view to get user data by token

--- TweetGymAPI/tweetgym/userprofile/urls.py
+++ TweetGymAPI/tweetgym/userprofile/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProfileViewSet
+from .views import ProfileViewSet, UserDetailView
 
 router = routers.DefaultRouter()
 router.register('', ProfileViewSet)
 
 urlpatterns = [
+    path('detail/', UserDetailView.as_view()),
     path('', include(router.urls)),
 ]
